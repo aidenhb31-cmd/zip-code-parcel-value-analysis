e76aae3d2f927814c6fcc45606c00a8d4d6fdded
aidenhb31-cmd <aidenhb31@gmail.com>
2026-08-09 11:53:27 -0400

    Update 02_outlier_analysis.sql

--- sql/02_outlier_analysis.sql
+++ sql/02_outlier_analysis.sql
@@ -1,1 +1,18 @@
-
+------------------------
+Looks at MCITY, and finds where the CITY, STATE, ZIP+4 format is broken
+------------------------
+WITH BASE_QUERY AS (
+  SELECT 
+    MCITY,
+    REGEXP_EXTRACT(MCITY, r'^(.*)\s+[A-Z]{2}\s+\d{5}(?:-\d{4})?$') AS city_clean,
+    REGEXP_EXTRACT(MCITY, r'^.*\s+([A-Z]{2})\s+\d{5}(?:-\d{4})?$') AS state_clean,
+    REGEXP_EXTRACT(MCITY, r'^.*\s+[A-Z]{2}\s+(\d{5}(?:-\d{4})?)$') AS zip_clean
+  FROM `project.mydataset.table` 
+)
+SELECT MCITY, COUNT(*) AS cnt
+FROM BASE_QUERY
+WHERE city_clean IS NULL
+OR state_clean IS NULL
+OR zip_clean IS NULL
+GROUP BY MCITY
+ORDER BY cnt DESC;
