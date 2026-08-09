d8b0cf91cf431c6d85878341ce6212cdcb1a563f
aidenhb31-cmd <aidenhb31@gmail.com>
2026-08-09 11:50:09 -0400

    Update 01_zip_parcel_analysis.sql

--- sql/01_zip_parcel_analysis.sql
+++ sql/01_zip_parcel_analysis.sql
@@ -1,3 +1,6 @@
+--------------------------
+Parcel Value Summary
+--------------------------
 SELECT
   MSTATE,
   REGEXP_EXTRACT(MZIP, r'^(\d{5})') AS zip_clean,
@@ -14,3 +17,27 @@ AND PARVAL IS NOT NULL
 AND PARVAL != 0
 GROUP BY zip_clean, MSTATE
 ORDER BY parcel_count DESC;
+------------------------------------
+Top Zip Codes by Median Parcel Value
+------------------------------------
+SELECT
+  MSTATE,
+  REGEXP_EXTRACT(MZIP, r'^(\d{5})') AS zip_clean,
+  COUNT(*) AS parcel_count,
+  ROUND(AVG(PARVAL)) AS avg_parcel_value,
+  APPROX_QUANTILES(PARVAL, 100)[OFFSET(50)] AS median_parcel_value,
+  SUM(PARVAL) AS total_parcel_value
+FROM `project.dataset.table`
+WHERE MZIP IS NOT NULL
+  AND MSTATE = "NC"
+  AND PARVAL IS NOT NULL
+  AND PARVAL != 0
+GROUP BY
+  MSTATE,
+  zip_clean
+HAVING COUNT(*) >= 100
+ORDER BY
+  median_parcel_value DESC
+LIMIT 20;
+------------------------------------
+
